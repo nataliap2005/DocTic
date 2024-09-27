7176db4b808bcdfd4e407958d4928ff35c677252
María José Suárez <maria_jos.suarez@uao.edu.co>
2024-09-27 10:58:58 -0500

    Desarrollo parcial del CRUD historial_contraseña

---
+++ src/main/java/com/DocTIC/DocTic/Repository/IHistorialcontrasenaRepository.java
@@ -0,0 +1,8 @@
+package com.DocTIC.DocTic.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.DocTIC.DocTic.Model.HistorialContrasenaModel;
+
+public interface IHistorialcontrasenaRepository extends JpaRepository <HistorialContrasenaModel, Integer> {
+}
