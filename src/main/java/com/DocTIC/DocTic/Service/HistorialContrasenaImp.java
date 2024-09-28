e0ac590d537371196e0b92ed268b6c2365fd82f9
María José Suárez <maria_jos.suarez@uao.edu.co>
2024-09-27 19:00:10 -0500

    CRUD historial contraseña finalizado :3

--- src/main/java/com/DocTIC/DocTic/Service/HistorialContrasenaImp.java
+++ src/main/java/com/DocTIC/DocTic/Service/HistorialContrasenaImp.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
+import java.util.List;
 import com.DocTIC.DocTic.Exception.RecursoNoEncontradoException;
 import com.DocTIC.DocTic.Model.HistorialContrasenaModel;
 import com.DocTIC.DocTic.Repository.IHistorialcontrasenaRepository;
@@ -29,4 +30,27 @@ public class HistorialContrasenaImp implements IHistorialcontrasenaService {
         return historial.orElseThrow(()-> new RecursoNoEncontradoException("¡Error! El historial contraseña del id no fue encontrado o es erróneo."));
     };
 
+    @Override
+    public List<HistorialContrasenaModel> listarHistorialContrasena() {
+        return historialcontrasenaRepository.findAll();
+    
+    }
+    @Override
+    public String editarHistContrasena(HistorialContrasenaModel hcontrasena){
+        Optional<HistorialContrasenaModel> HistorialContrasenaEcontrado = historialcontrasenaRepository.findById(hcontrasena.getIdHistorial());
+
+        if (HistorialContrasenaEcontrado.isPresent()){
+            historialcontrasenaRepository.save(hcontrasena);
+            return "El historial contraseña con ID "+ hcontrasena.getIdHistorial()+" fue editado con exito";
+        } else{
+            throw new RecursoNoEncontradoException("El historial de contraseña con ID " + hcontrasena.getIdHistorial() +" no se encuentra en la base de datos o es erróneo." );
+        }
+    }
+    @Override
+    public void eliminarHistContrasenaPorId(int historialContrasenaId) {
+        historialcontrasenaRepository.deleteById(historialContrasenaId);
+    }   
 }
+
+
+
--- src/main/java/com/DocTIC/DocTic/Service/IHistorialcontrasenaService.java
+++ src/main/java/com/DocTIC/DocTic/Service/IHistorialcontrasenaService.java
@@ -1,10 +1,18 @@
 package com.DocTIC.DocTic.Service;
-
 import com.DocTIC.DocTic.Model.HistorialContrasenaModel;
+
+import java.util.List;
 
 public interface IHistorialcontrasenaService {
     String insertarhistorialContrasena(HistorialContrasenaModel historialContrasenaData);
     
     HistorialContrasenaModel obtenerHistContrasena (int historialContrasenaId);
 
+    List <HistorialContrasenaModel> listarHistorialContrasena();
+
+    String editarHistContrasena(HistorialContrasenaModel historialContrasena);
+    
+    void eliminarHistContrasenaPorId(int historialContrasenaId);
 }
+
+
